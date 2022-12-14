d46f333eebb78453530d887203bf1a2faf366869
Nouriah <112393357+noouriah@users.noreply.github.com>
2022-12-14 23:46:58 +0300

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,13 @@
+// مصفوفه تطبع من واحد ال عشره
+public class Ex1 {
+    public static void main(String[] args) {
+     //   for (int i=1;i<=10;i++){
+      //      System.out.println(i);
+      //  }
+        int a[];
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+                
+    }
+}
---
+++ Ex4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex4 {
+    public void input(){
+    int[]a=new int[5];
+     Scanner in=new Scanner(System.in);
+      for(int i=0;i<5;i++){
+     a[i]=in.nextInt();
+     }}
+
+}
